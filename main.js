c9efebed6fe1a90a72e7a0e61d038d355ff7c4ba
minimario <gua@mit.edu>
2022-01-23 18:13:14 -0500

    comment out schemas we have a lot of data for

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ var app = new Vue({
         submitted: false,
         prompts: [
             "I want a company management app where I can track my employees, their schedules, and their compensation."
-            , "I want a pet store app tracking customers. The pet store app should also keep the store's inventory of pets and allow customers to buy and sell pets."
+            // , "I want a pet store app tracking customers. The pet store app should also keep the store's inventory of pets and allow customers to buy and sell pets."
             , "I want a restaurant management app tracking customers, their reservations, their orders, and menu items."
             , "I want a hotel app tracking guests, their reservations. The hotel app should also track which guests occupy which rooms."
-            , "I want a dating app where students can indicate other students they are interested in, and the dating app will match them."
+            // , "I want a dating app where students can indicate other students they are interested in, and the dating app will match them."
             , "I want a library app to track books. The customers can borrow books and return books from the library."
             , "I want a concert management app that lists concerts, their dates, and their locations. The customers can also buy tickets to concerts."
             // , "I want a classroom management app that tracks students, the assignments they've submitted, and the grades they've earned on those assignments."
@@ -32,7 +32,7 @@ var app = new Vue({
         },
         saveField(index, item, fieldName, fieldType) {
             this.items[index].fields[item].fieldName = fieldName,
-            this.items[index].fields[item].fieldType = fieldType
+                this.items[index].fields[item].fieldType = fieldType
         },
         deleteField(index, fieldIndex) {
             this.items[index].fields.splice(fieldIndex, 1);
@@ -58,18 +58,18 @@ var app = new Vue({
             this.items[index].edit = false;
         },
         editAll() {
-            for (var index=0; index < this.items.length; index++) {
+            for (var index = 0; index < this.items.length; index++) {
                 this.items[index].edit = true;
             }
         },
         saveAll() {
-            for (var index=0; index < this.items.length; index++) {
+            for (var index = 0; index < this.items.length; index++) {
                 this.items[index].edit = false;
             }
         },
         submitAll() {
             let data = {
-                prompt: this.selectedPrompt, 
+                prompt: this.selectedPrompt,
                 submission: JSON.stringify(this.items)
             };
 
@@ -88,7 +88,7 @@ var app = new Vue({
             this.items = [];
             this.addTable();
             this.submitted = true;
-            
+
             prompt = this.selectedPrompt;
             while (prompt == this.selectedPrompt) {
                 prompt = this.prompts[Math.floor(Math.random() * this.prompts.length)];
